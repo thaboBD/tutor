f2056954739b0ab3809fcfd0df67c11f3d385400
Thabo Dube <thabodube@Thabos-MacBook-Air.local>
2023-11-06 21:41:43 +1100

    added mathpix endpoint for reading images

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
-from fastapi import UploadFile, File, FastAPI
+from fastapi import FastAPI, File, UploadFile, HTTPException
+from fastapi.responses import JSONResponse
 from typing import List
 from pdf_util import extract_pdf_text
 # from semantic_search import get_answer
@@ -6,12 +7,17 @@ from pdf_util import extract_pdf_text
 from langchain_util import run_agent as calculated
 from pydantic import BaseModel
 import logging
+import os
+from datetime import datetime
+from mathpix import readImage
 
 logging.basicConfig(filename='app.log', level=logging.INFO,
                     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
 
 app = FastAPI()
+
+os.makedirs('images', exist_ok=True)
 
 # This will store our content indexed by some identifier
 content_index = {}
@@ -54,3 +60,23 @@ def search(query: QueryModel):
 async def calculate(query: QueryModel):
     results = calculated(query.query)
     return results
+
+
+@app.post("/read-image/")
+async def upload_image(file: UploadFile = File(...)):
+    if not file.content_type.startswith('image/'):
+        raise HTTPException(
+            status_code=400, detail=f'File type not supported: {file.content_type}')
+
+    try:
+        contents = await file.read()
+        timestamp = datetime.utcnow().strftime('%Y%m%d%H%M%S%f')
+        filename = f"images/{timestamp}_{file.filename}"
+
+        with open(filename, 'wb') as f:
+            f.write(contents)
+
+        image_string = readImage(filename)
+        return JSONResponse(status_code=200, content={"filename": file.filename, "image_string": image_string})
+    except Exception as e:
+        return JSONResponse(status_code=500, content={"message": str(e)})
