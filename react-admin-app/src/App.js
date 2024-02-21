58c81640df60144c2b09c1618db8430fae8eebaf
abzuar9658 <abuzar9658@gmail.com>
2024-02-21 21:25:33 +0400

    unable to read still;

--- react-admin-app/src/App.js
+++ react-admin-app/src/App.js
@@ -13,6 +13,7 @@ import {
 const UsersApi = `${process.env.REACT_APP_API_URL}/api/v1/users/login`;
 const authToken = localStorage.getItem("token");
 
+console.log("USERS API", process.env)
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(!!authToken);
   const [email, setEmail] = useState("");
@@ -26,7 +27,7 @@ function App() {
     try {
       const response = await axios.post(UsersApi, { email, password });
       const { token } = response.data;
-      console.log("TOKEN: ", token);
+      console.log("MY TOKEN: ", token);
       localStorage.setItem("token", token);
       setIsLoggedIn(true);
     } catch (error) {
